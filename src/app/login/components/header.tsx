9b4891b3693684c945ab96836ea8010b59ff45f3
HenriqueBragaMoreira <shenrique40moreira@gmail.com>
2024-03-14 15:56:45 -0300

    feat: added logo on header of login and added a favicon to application

--- src/app/login/components/header.tsx
+++ src/app/login/components/header.tsx
@@ -1,9 +1,14 @@
 import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+import logo from '@/../public/logoZurimpy.png';
 
 export function Header() {
   return (
     <div className="flex justify-between">
-      <h1 className="font-bold text-xl">Zurimpy</h1>
+      <a href="/" className="flex gap-2 items-center">
+        <Image src={logo} alt="Logo da empresa Zurimpy" width={40} height={40} />
+        <h1 className="font-bold text-xl">Zurimpy</h1>
+      </a>
       <div className="flex gap-4">
         <Button className="bg-gray-100 hover:bg-gray-300 text-black">Explore Features</Button>
         <Button>Get Started</Button>
