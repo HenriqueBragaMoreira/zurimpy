c7f5e3c37d418a6e4f9dc43e17d3f879d8341cd1
HenriqueBragaMoreira <shenrique40moreira@gmail.com>
2024-03-14 15:35:15 -0300

    feat(sidebar): added logo of Zurimpy

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,7 +1,9 @@
 'use client';
 import { routes } from '@/utils/routes';
+import Image from 'next/image';
 import { useRouter, usePathname } from 'next/navigation';
 import { useState } from 'react';
+import logo from '../../public/logoZurimpy.png';
 
 export function Sidebar() {
   const [active, setActive] = useState(usePathname());
@@ -13,13 +15,16 @@ export function Sidebar() {
   }
 
   return (
-    <div className="flex flex-col items-center py-4 px-4 bg-gray-200 w-64 gap-10">
-      <h1 className="font-bold text-2xl">Zurimpy</h1>
+    <div className="flex flex-col items-center py-4 px-4 bg-gray-300 w-64 gap-10">
+      <div className="flex gap-2 items-center">
+        <Image src={logo} alt="logo" height={50} width={50} />
+        <h1 className="font-bold text-2xl">Zurimpy</h1>
+      </div>
       <div className="w-full flex flex-col gap-4">
         {routes.map((item) => (
           <div
             onClick={() => handlePush(item.path)}
-            className={`flex gap-4 hover:bg-gray-300 font-semibold py-2 px-3 rounded-md items-center cursor-pointer ${item.path === active ? 'bg-gray-300 text-black' : 'text-gray-400'}`}
+            className={`flex gap-4 hover:bg-gray-400 font-semibold py-2 px-3 rounded-md items-center cursor-pointer ${item.path === active ? 'bg-gray-400 text-black' : 'text-gray-500'}`}
             key={item.id}
           >
             <span>
